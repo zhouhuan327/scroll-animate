20b5b05dd02a2329bbe806110d2a0166a073d481
zhouhuan <zsean21598@163.com>
2022-02-11 18:05:06 +0800

    初始化

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,76 @@
-import "./ScrollAnimate";
+import * as Three from "three";
+import type { PerspectiveCamera, Scene, WebGLRenderer } from "three";
+class ScrollAnimate {
+  container: HTMLElement;
+  scene: Scene;
+  renderer: WebGLRenderer;
+  camera: PerspectiveCamera;
+  sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+  constructor(targetClassName = "canvas.webgl") {
+    this.container = document.querySelector(targetClassName);
+    this.init();
+  }
+  private init() {
+    // 初始化场景
+    this.scene = new Three.Scene();
+    // 相机
+    this.setCamera();
+    // 渲染器
+    this.setRenderer();
+    // 动画相关
+    this.setAnimate();
+    // 响应式
+    this.setResponsive();
+  }
+  setCamera() {
+    const camera = new Three.PerspectiveCamera(
+      75,
+      this.sizes.width / this.sizes.height,
+      0.1,
+      100
+    );
+    camera.position.z = 6;
+    this.scene.add(camera);
+    this.camera = camera;
+  }
+  setRenderer() {
+    const renderer = new Three.WebGLRenderer({
+      canvas: this.container,
+    });
+    renderer.setSize(this.sizes.width, this.sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    this.renderer = renderer;
+  }
+  setAnimate() {
+    const clock = new Three.Clock();
+    const tick = () => {
+      const elapsedTime = clock.getElapsedTime();
+
+      // Render
+      this.renderer.render(this.scene, this.camera);
+
+      // Call tick again on the next frame
+      window.requestAnimationFrame(tick);
+    };
+    tick();
+  }
+  setResponsive() {
+    window.addEventListener("resize", () => {
+      // Update sizes
+      this.sizes.width = window.innerWidth;
+      this.sizes.height = window.innerHeight;
+
+      // Update camera
+      this.camera.aspect = this.sizes.width / this.sizes.height;
+      this.camera.updateProjectionMatrix();
+
+      // Update renderer
+      this.renderer.setSize(this.sizes.width, this.sizes.height);
+      this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    });
+  }
+}
+export default ScrollAnimate;
